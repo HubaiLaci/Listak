bbd8c34df94e7f810d0704aa4fe868161be450cc
Lackó <113759893+HubaiLaci@users.noreply.github.com>
2022-10-03 14:16:21 +0200

    Add files via upload

---
+++ listák.py
@@ -0,0 +1,13 @@
+honapok = ['január', 'február', 'március', 'április']
+
+print (honapok)
+print(('|').join(honapok))
+
+print(len(honapok))
+print(honapok[1:3])
+print(honapok[0:2])
+print(honapok[-2])
+
+szo = 'almafa'
+print(szo[:4])
+
